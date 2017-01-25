9505b1bda9fe2f292d2ef00576bb2feb91eba7be
aiday0503 <aiday.dodonya@gmail.com>
2017-01-25 00:03:49 -0800

    Created sample of colors
    modified colors in CSS

--- animation.js
+++ animation.js
@@ -8,7 +8,31 @@ $(document).ready(function(){
     $(".toggleBar2").click(function(){
         $("#toggleContent2").animate({
             height: 'toggle'
-        })
-
-    })
+        });
+    });
+    $(".toggleBar3").click(function(){
+         $("#toggleContent3").animate({
+             height: 'toggle'
+        });
+    });
+    $(".toggleBar4").click(function(){
+        $("#toggleContent4").animate({
+            height: 'toggle'
+        });
+    });
+    $(".toggleBar5").click(function(){
+        $("#toggleContent5").animate({
+            height: 'toggle'
+        });
+    });
+    $(".toggleBar6").click(function(){
+        $("#toggleContent6").animate({
+            height: 'toggle'
+        });
+    });
+    $(".toggleBar7").click(function(){
+        $("#toggleContent7").animate({
+            height:'toggle'
+        });
+    });
 });
